ecd2a0f6e0ab12f432f83f3acba3f04d0a4cba15
ZodexNL <zodexnl@gmail.com>
2021-11-04 14:36:02 +0100

    Inlog is nu compleet werkend. Het uitloggen ook

--- index.php
+++ index.php
@@ -1,1 +1,34 @@
 <?php
+session_start();
+if($_SESSION["ingelogd"] == false){
+    echo "<script>window.alert('Je moet eerst inloggen!')</script>";
+    echo "<script>window.location = 'inlog.php'</script>";
+}else{
+    ?>
+    <html>
+<head>
+    <title>Test</title>
+    <form method="post">
+        <input type="submit" name="uitloggen" placeholder="uitloggen">
+    </form>
+
+</head>
+<body>
+
+</body>
+</html>
+
+<?php
+echo "Welkom " . $_SESSION["naam"];
+
+if(isset($_POST["uitloggen"])){
+    session_unset();
+    session_destroy();
+    echo "<script>window.alert('Je bent succesvol uitgelogd!')</script>";
+    echo "<script>window.location = 'inlog.php'</script>";
+}
+
+
+}
+
+?>
--- inlog.php
+++ inlog.php
@@ -8,7 +8,7 @@
     <form method="post">
         <input type="text" id="naam" name="naam">
         <input type="password" id="wachtwoord" name="password">
-        <input type="submit" name="submit" placeholder="Submit">
+        <input type="submit" name="login" placeholder="Submit" value="Login">
     </form>
 
 </body>
@@ -16,11 +16,41 @@
 
 <?php
 
-if(isset($_POST["submit"])){
-    $naam = $_POST["naam"];
-    $wachtwoord = $_POST["password"];
-
+if(isset($_POST['login'])){
     include ("database.php");
 
-    $query = ("SELECT * FROM users WHERE naam = '$naam' AND wachtwoord = '$wachtwoord'");
+
+    $name = $_POST['naam'];
+    $pass = $_POST['password'];
+
+
+    $sth = $conn->prepare("SELECT * FROM users");
+    $info2 = $sth->execute();
+    $info = $sth->setFetchMode(PDO::FETCH_OBJ);
+
+
+
+    try{
+        foreach ($sth->fetchAll(PDO::FETCH_OBJ) as $info){
+
+            if ($name == $info->naam && $pass == $info->wachtwoord){
+                session_start();
+                $_SESSION["naam"] = $name;
+                $_SESSION["ingelogd"] = true;
+                echo "Succesvol ingelogd";
+
+                echo "<script>window.location = 'index.php'</script>";
+
+            }else {
+                echo "het gaat niet goed";
+            }
+        }
+    }catch (Exception $er){
+        echo $er->getMessage();
+    }
+
+
 }
+
+?>
+
