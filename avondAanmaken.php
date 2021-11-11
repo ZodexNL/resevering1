af330ca03844e5ba3d5df97108ccbff8af3c37dc
ZodexNL <zodexnl@gmail.com>
2021-11-11 14:26:07 +0100

    poging tot hoeveelheid

--- avondAanmaken.php
+++ avondAanmaken.php
@@ -17,6 +17,47 @@ if($_SESSION["ingelogd"] == false){
 </head>
 <body>
 <h1>Nieuwe ouderavond aanmaken</h1>
+
+<form method="post">
+    <label for="datum">Datum:</label>
+    <input type="date" name="datum">
+
+    <label>van </label>
+    <input type="time" name="tijd1Van">
+    <label> tot </label>
+    <input type="time" name="tijd1Tot">
+
+        <select id="hoeveelheid" name="hoeveelheid" onchange="<?php bepaal();?>">
+            <option value="1"></option>
+            <option value="2"></option>
+            <option value="3"></option>
+            <option value="4"></option>
+            <option value="5"></option>
+            <option value="6"></option>
+            <option value="7"></option>
+            <option value="8"></option>
+            <option value="9"></option>
+            <option value="10"></option>
+
+        </select>
+
+    <?php
+    function bepaal(){
+        $hoeveelheid = $_POST["hoeveelheid"];
+
+
+        for($i = 0; $i < $hoeveelheid; $i++){
+            echo "<label> van </label> <input type='time' name='van$i'><label> tot </label> <input type='time' name='tot$i'>";
+        }
+    }
+
+
+    ?>
+
+
+
+</form>
+
 </body>
 </html>
 
